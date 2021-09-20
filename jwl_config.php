624e297ecebc8cf633742c617dd62880ce20c9e2
invintsLLP <jagrutivekariya261@gmail.com>
2021-09-20 15:07:11 +0530

    first commit

---
+++ jwl_config.php
@@ -0,0 +1,9 @@
+<?php
+/*
+* @ Eskayvie: Private App Details(Api Key, Password, Domain and Url).
+*/
+define("SHOPIFY_API_KEY","519c81dbb0364364d60a33c8dcbcdb43");
+define("SHOPIFY_API_PASSWORD","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");
+define("API_DOMAIN","grandeur-co-us.myshopify.com");
+define('API_URL', 'https://' . SHOPIFY_API_KEY . ':' . SHOPIFY_API_PASSWORD . '@' . API_DOMAIN . '/');
+?>
